2c53ddc33d00516e09371b543e95648ead5a8f81
newagebegins <newagebegins@gmail.com>
2023-05-01 20:00:22 +1000

    Implement wcat

---
+++ projects/initial-utilities/wcat/wcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+enum { buf_size = 512 };
+
+int main(int argc, char* argv[argc+1]) {
+  for (size_t i = 1; i < argc; ++i) {
+    FILE* fp = fopen(argv[i], "r");
+    if (!fp) {
+      printf("wcat: cannot open file\n");
+      return 1;
+    }
+    char buf[buf_size];
+    while (fgets(buf, buf_size, fp)) {
+      fputs(buf, stdout);
+    }
+  }
+  return 0;
+}
