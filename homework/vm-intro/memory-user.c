09f30c8f88301155afb2e082fc8d385548aab86b
newagebegins <newagebegins@gmail.com>
2023-05-08 06:50:21 -0400

    Faster array filling

--- homework/vm-intro/memory-user.c
+++ homework/vm-intro/memory-user.c
@@ -15,13 +15,14 @@ int main(int argc, char* argv[argc+1]) {
   }
   size_t szb = (size_t)szmb * 1024 * 1024;
   printf("szmb = %d, szb = %zu, SIZE_MAX = %zu\n", szmb, szb, SIZE_MAX);
-  unsigned char* buf = malloc(szb);
+  unsigned long* buf = malloc(szb);
   if (!buf) {
     fprintf(stderr, "malloc failed\n");
     return 1;
   }
+  size_t len = szb / sizeof(unsigned long);
   while (1) {
-    for (size_t i = 0; i < szb; ++i) {
+    for (size_t i = 0; i < len; ++i) {
       buf[i] = 255;
     }
     sleep(1);
