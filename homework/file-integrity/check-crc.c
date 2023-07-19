ec1d4d16478a83907bc4c69b5013dee163219204
newagebegins <newagebegins@gmail.com>
2023-07-19 14:04:56 +1000

    Measure performance of check-crc

--- homework/file-integrity/check-crc.c
+++ homework/file-integrity/check-crc.c
@@ -5,8 +5,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-#define BUF_SIZE 128
+#define BUF_SIZE (4*1024)
+#define Gettimeofday(a,b) assert(gettimeofday(a,b) == 0)
 
 int main(int argc, char* argv[argc+1]) {
   assert(argc == 2);
@@ -17,6 +19,9 @@ int main(int argc, char* argv[argc+1]) {
   }
   unsigned char buf[BUF_SIZE];
   ssize_t num_read = 0;
+
+  struct timeval start;
+  Gettimeofday(&start, 0);
 
   /*
     https://ru.wikibooks.org/wiki/%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8_%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%BE%D0%B2/%D0%A6%D0%B8%D0%BA%D0%BB%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9_%D0%B8%D0%B7%D0%B1%D1%8B%D1%82%D0%BE%D1%87%D0%BD%D1%8B%D0%B9_%D0%BA%D0%BE%D0%B4#CRC-16
@@ -40,11 +45,18 @@ int main(int argc, char* argv[argc+1]) {
     }
   }
 
+  struct timeval end;
+  Gettimeofday(&end, 0);
+
   if (num_read < 0) {
     perror("read failed");
     return EXIT_FAILURE;
   }
 
   printf("0x%X\n", crc);
+
+  double secs = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) * 1E-6;
+  printf("time = %g secs\n", secs);
+
   return EXIT_SUCCESS;
 }
