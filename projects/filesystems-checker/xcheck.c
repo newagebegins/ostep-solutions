974f1df0b9f7f1d03c5fffa986b5e619512e4ece
newagebegins <newagebegins@gmail.com>
2023-07-23 00:04:12 -0400

    xcheck: checks 6-8

--- projects/filesystems-checker/xcheck.c
+++ projects/filesystems-checker/xcheck.c
@@ -15,6 +15,8 @@
 #undef stat
 #undef dirent
 
+typedef uchar dblock[BSIZE];
+
 int main(int argc, char* argv[argc+1]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: xcheck <file_system_image>\n");
@@ -39,11 +41,11 @@ int main(int argc, char* argv[argc+1]) {
     return 1;
   }
 
-  struct superblock* sb = (struct superblock*) (fs + BSIZE);
+  dblock* blocks = (dblock*) fs;
 
-  uint num_inode_blocks = (sb->ninodes + IPB - 1)/IPB; // round up
-  uint num_bitmap_blocks = (sb->size + BPB - 1)/BPB; // round up
-  uint data_start = 2 + num_inode_blocks + num_bitmap_blocks;
+  struct superblock* sb = (struct superblock*) blocks[1];
+
+  uint data_start = BBLOCK(sb->size, sb->ninodes) + 1;
   uint data_end = sb->size - 1;
 
   struct dinode* inodes = (struct dinode*) (fs + IBLOCK(0)*BSIZE);
@@ -55,6 +57,8 @@ int main(int argc, char* argv[argc+1]) {
     int m = 1 << (bi % 8);
     bitmap[b] = !!(bp[bi/8] & m);
   }
+
+  ushort* block_to_inode = calloc(sb->nblocks, sizeof(ushort));
 
   for (ushort inum = 0; inum < sb->ninodes; ++inum) {
     struct dinode* ip = &inodes[inum];
@@ -74,6 +78,11 @@ int main(int argc, char* argv[argc+1]) {
             fprintf(stderr, "ERROR: address used by inode but marked free in bitmap\n");
             return 1;
           }
+          if (block_to_inode[block]) {
+            fprintf(stderr, "ERROR: direct address used more than once\n");
+            return 1;
+          }
+          block_to_inode[block] = inum;
         }
       }
       if (ip->addrs[NDIRECT]) {
@@ -81,8 +90,15 @@ int main(int argc, char* argv[argc+1]) {
           fprintf(stderr, "ERROR: bad indirect address in inode\n");
           return 1;
         }
-        uint* indirect_addrs = (uint*) (fs + ip->addrs[NDIRECT]*BSIZE);
-        for (uint i = 0; i < BSIZE/sizeof(uint); ++i) {
+
+        if (block_to_inode[ip->addrs[NDIRECT]]) {
+          fprintf(stderr, "ERROR: indirect address used more than once\n");
+          return 1;
+        }
+        block_to_inode[ip->addrs[NDIRECT]] = inum;
+
+        uint* indirect_addrs = (uint*) blocks[ip->addrs[NDIRECT]];
+        for (uint i = 0; i < NINDIRECT; ++i) {
           uint block = indirect_addrs[i];
           if (block) {
             if (block < data_start || block > data_end) {
@@ -93,6 +109,11 @@ int main(int argc, char* argv[argc+1]) {
               fprintf(stderr, "ERROR: address used by inode but marked free in bitmap\n");
               return 1;
             }
+            if (block_to_inode[block]) {
+              fprintf(stderr, "ERROR: indirect address used more than once\n");
+              return 1;
+            }
+            block_to_inode[block] = inum;
           }
         }
       }
@@ -102,7 +123,7 @@ int main(int argc, char* argv[argc+1]) {
         fprintf(stderr, "ERROR: directory not properly formatted\n");
         return 1;
       }
-      struct xv6_dirent* dir = (struct xv6_dirent*) (fs + ip->addrs[0]*BSIZE);
+      struct xv6_dirent* dir = (struct xv6_dirent*) blocks[ip->addrs[0]];
       if (dir->inum != inum || strcmp(dir->name, ".") != 0) {
         fprintf(stderr, "ERROR: directory not properly formatted\n");
         return 1;
@@ -119,7 +140,7 @@ int main(int argc, char* argv[argc+1]) {
     fprintf(stderr, "ERROR: root directory does not exist\n");
     return 1;
   }
-  struct xv6_dirent* root_dir = (struct xv6_dirent*) (fs + root_inode->addrs[0]*BSIZE);
+  struct xv6_dirent* root_dir = (struct xv6_dirent*) blocks[root_inode->addrs[0]];
   if (root_dir->inum != ROOTINO || strcmp(root_dir->name, ".") != 0) {
     fprintf(stderr, "ERROR: root directory does not exist\n");
     return 1;
@@ -127,6 +148,13 @@ int main(int argc, char* argv[argc+1]) {
   if ((root_dir+1)->inum != ROOTINO || strcmp((root_dir+1)->name, "..") != 0) {
     fprintf(stderr, "ERROR: root directory does not exist\n");
     return 1;
+  }
+
+  for (uint b = data_start; b < sb->size; ++b) {
+    if (bitmap[b] && !block_to_inode[b]) {
+      fprintf(stderr, "ERROR: bitmap marks block in use but it is not in use\n");
+      return 1;
+    }
   }
 
   return 0;
